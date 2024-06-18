1fdd9908b8393ee5bd4b17191c296283ae237d23
muhammedshamilv <shamil@hamon.in>
2024-06-18 11:24:44 +0530

    adds cors

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -4,17 +4,25 @@ from app.models import User
 from sqlalchemy.exc import IntegrityError
 from app.database import get_db
 from jose import jwt
-from app.settings import JWT_SECRET_KEY, JWT_ALGORITHM,ACCESS_TOKEN_EXPIRE_MINUTES,REFRESH_TOKEN_EXPIRE_DAYS
+from app.settings import JWT_SECRET_KEY, JWT_ALGORITHM
 from app.middleware.auth import auth_required, get_current_user
 from app.utils import authenticate_user,create_access_token
+from pydantic import BaseModel, EmailStr
+
 import logging
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
+class LoginRequest(BaseModel):
+    email: EmailStr
+    password: str
+    
 @router.post("/login")
-async def login(email,password):    
+async def login(login_data: LoginRequest):
+    email = login_data.email
+    password = login_data.password  
     user = await authenticate_user(email, password)
     if not user:
         logger.error("user missing")
@@ -30,8 +38,10 @@ async def login(email,password):
     return access_token
 
 @router.post("/register")
-async def register(email,password,db: Session = Depends(get_db)):
+async def register(data: LoginRequest,db: Session = Depends(get_db)):
     try:
+        email = data.email
+        password = data.password  
         hashed_password = User.hash_password(password)
         user = User(email=email, hashed_password=hashed_password)
         db.add(user)
